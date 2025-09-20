590b3effb57fb8b8d34c7fed3d43c3f3900d2e1f
edgarterrones <terronee1837@student.faytechcc.edu>
2025-09-20 16:19:48 -0400

    Add files via upload

---
+++ P1LAB1_TerronesEdgar.py
@@ -0,0 +1,13 @@
+# Edgar C Terrones
+# 09/20/25
+# P1LAB1
+# Welcome to Python
+
+print("Enter your first name:")
+first_name = input()
+
+
+print("Enter your last name:")
+last_name = input()
+
+print("Hello!", first_name, last_name, "Welcome to CTI-110")
