cae550c6047e1d50d9599d6e693e48f17f4170a8
edgarterrones <terronee1837@student.faytechcc.edu>
2025-09-23 14:40:28 -0400

    Add files via upload

--- P1HW2_TerronesEdgar.py
+++ P1HW2_TerronesEdgar.py
@@ -3,21 +3,29 @@
 # P1HW2
 # Travel Budget
 
+# This saves users budget
 print("Please enter your travel budget:")
 budget = int(input())
+# This saves users travel location
 print("Please enter your travel destination:")
 location = input()
+# This saves how much you will spend on gas
 print("How much will you spend on gas:")
 gas = int(input())
+# This saves how much you will spend on gas
 print("How much will you spend on accomadation:")
 hotel = int(input())
+# This saves how much you will spend on food
 print("How much will you spend on food:")
 food = int(input())
+
+# This will give you total expenses based on the information inputed
 total = gas + hotel + food
 print("Total Expenses =", total)
+# This subtracts your travel budge by your total expected cost in order to provide your remaining budget
 remaining = budget - total
 
-
+#This will print out a list of total expenses and remaining balance
 print("-----Total Expenses-----")
 print("Location:", location)
 print("Initial Budget:", budget)
