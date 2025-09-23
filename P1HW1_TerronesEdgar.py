c8658207af53c8bf49392825ec2c0251c3206745
edgarterrones <terronee1837@student.faytechcc.edu>
2025-09-23 12:43:42 -0400

    Add files via upload

---
+++ P1HW1_TerronesEdgar.py
@@ -0,0 +1,28 @@
+# Edgar C Terrones
+# 09/23/25
+# P1HW1
+# Math Program
+
+print("-----Calculating Exponents-----")
+
+print("Enter an integer as the base value:")
+base = int(input())
+
+print("Enter an interger as the exponent:")
+exponent = int(input())
+
+print(base, "raised to the power of", exponent, "is", base ** exponent, "!!")
+
+print("-----Addition and Subtraction-----")
+
+print("Enter a starting integer:")
+a = int(input())
+
+print("Enter an integer to add:")
+b = int(input())
+
+print("Enter an integer to subtract:")
+c = int(input())
+
+print(a, "+", b, "-",c, "is", a + b - c)
+
