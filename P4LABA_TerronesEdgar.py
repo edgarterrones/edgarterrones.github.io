8021d69bc55f1d97e181a46eb543366b2656bd3d
edgarterrones <terronee1837@student.faytechcc.edu>
2025-10-22 14:16:53 -0400

    Add files via upload

---
+++ P4LABA_TerronesEdgar.py
@@ -0,0 +1,17 @@
+# Edgar C Terrones
+# 10/21/25
+# P4LAB1A
+# TURTLE
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(4):
+    t.forward(100)  # Move forward by 100 units
+    t.left(90) # turns at 90 degree angle
+
+for i in range(3):
+    t.forward(100)
+    t.left(120) #turns at 120 degree angle
+    
+turtle.done()
